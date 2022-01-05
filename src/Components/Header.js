faccec7c03a508f8eda4d43783c7260d3b8c5967
sara.kuenzi <48669509+skuenzi24@users.noreply.github.com>
2022-01-05 16:18:28 -0700

    header

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import {GiWorld} from 'react-icons/gi'
+
+export default function Header () {
+    return (
+        <header>
+            <GiWorld className='header-icon'/>
+            <h4 className='header-title'>my travel journal.</h4>
+        </header>
+    )
+
+}
