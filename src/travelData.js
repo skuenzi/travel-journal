3bf570e7259cd75d63494fbeb09108f7ab5ff234
sara.kuenzi <48669509+skuenzi24@users.noreply.github.com>
2022-01-05 15:21:21 -0700

    cleaned up files and created data file

---
+++ src/travelData.js
@@ -0,0 +1,29 @@
+export default travelData = [
+    {
+        title: "Machapuchare (Fishtail)",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/tLt6MndPapzLJmxaA",
+        startDate: "24 Feb 2019",
+        endDate: "5 Mar, 2019",
+        description: "Machapuchare, or Fishtail Mountain, is part of the Annapurna range. We did the Mardi Himal trek, which took us from village to village in the foothills of the Annapurna range and gave us some magnificent view of the mountains.",
+        imageUrl: "./images/Fishtail.jpg"
+    },
+    {
+        title: "Hobbiton",
+        location: "New Zealand",
+        googleMapsUrl: "https://g.page/hobbiton-movie-set?share",
+        startDate: "7 Mar, 2017",
+        endDate: "",
+        description: "Picturesque sheep farm used for filming Shire scenes, with guided tours, a gift shop, cafe & inn.",
+        imageUrl: "./images/Hobbiton.jpg"
+    },
+    {
+        title: "Pu Luong Nature Reserve",
+        location: "Vietnam",
+        googleMapsUrl: "https://goo.gl/maps/KPEEQSxptap5UM5P7",
+        startDate: "20 Oct, 2018",
+        endDate: "23 Oct, 2018",
+        description: "Beautiful, mountainous nature reserve in northern Vietnam. I rented a motorbike and explored the area for a few days while staying at an absolutely heavenly homestay called Pu Luong Treehouse. Looking through my pictures for this project has made me yearn to go back here!",
+        imageUrl: "./images/PuLuong.jpg"
+    }
+]
